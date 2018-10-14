caf627d61749d35225fbea850c8a6b75558236e2
xMalware <xmalware2@gmail.com>
2018-10-14 10:06:12 +0200

    Add new server list

--- Model/Website.php
+++ Model/Website.php
@@ -62,13 +62,18 @@ class Website extends VoteAppModel
                 if ($result === false || intval($result) > 0)
                     return true;
                 break;
-	    case 'LIST-SRV-MC-ORG':
+	        case 'LIST-SRV-MC-ORG':
                 // Check with API
                 $result = @file_get_contents("http://www.liste-serveurs-minecraft.org/get_ip.php/{$website['data']['server_id']}/$ip");
                 if ($result === false || intval($result) > 0)
                     return true;
                 break;
-
+            case 'SRV-MULTIGAMES':
+                // Check with API
+                $result = @file_get_contents("https://serveur-multigames.net/api/{$website['data']['server_id']}/?ip=$ip");
+                if ($result === false || intval($result) > 0)
+                    return true;
+                break;
             default:
                 return true;
         }
