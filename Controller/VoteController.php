e7956817d855b60e8d8a4dea6e90768185771d00
Eywek <contact@eywek.fr>
2019-03-03 16:41:35 +0100

    improv: fix deleted_at

--- Controller/VoteController.php
+++ Controller/VoteController.php
@@ -19,12 +19,14 @@ class VoteController extends VoteAppController {
             $websitesByServers[$servers[$website['Website']['server_id']]][] = $website;
         }
         $this->set(compact('websitesByServers'));
-        $date = date('Y-m-d h:i:s');
         $this->set('users', array_map(function ($row) {
             return ['username' => $row['Vote']['username'], 'count' => $row[0]['count']];
         }, $this->Vote->find('all', [
             'fields' => ['username', 'COUNT(id) AS count'],
-            'conditions' => ['created LIKE' =>  date('Y') . '-' . date('m') . '-%', 'Vote.deleted_at >=' => $date],
+            'conditions' => [
+              'created LIKE' =>  date('Y') . '-' . date('m') . '-%',
+              'Vote.deleted_at' => null
+            ],
             'order' => 'count DESC',
             'group' => 'username',
             'limit' => 15
@@ -176,7 +178,6 @@ class VoteController extends VoteAppController {
         $reward = $this->Reward->getFromWebsite($website['Website']);
 
         // Store it
-        $date = 2642 . '-' . date('m-d h:i:s');
         $user = $this->Session->read('vote.user');
         $this->Vote->create();
         $this->Vote->set([
@@ -185,8 +186,7 @@ class VoteController extends VoteAppController {
             'reward_id' => $reward['id'],
             'collected' => 0,
             'website_id' => $website['Website']['id'],
-            'ip' => $this->Util->getIP(),
-            'deleted_at' => "$date"
+            'ip' => $this->Util->getIP()
         ]);
         $this->Vote->save();
 
