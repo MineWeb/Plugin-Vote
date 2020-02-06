b824bb4d2a6abb6e339ea66bf5dbce72133e73ea
nivcoo <36514752+nivcoo@users.noreply.github.com>
2020-02-06 18:15:00 +0100

    improv. rewrite a part of code which create lag (#41)
    * improv. rewrite a part of code
    
    * improv. rewrite a part of code

--- Controller/TopController.php
+++ Controller/TopController.php
@@ -5,42 +5,32 @@ class TopController extends VoteAppController
     {
         if (!$this->Permissions->can('VOTE__ADMIN_VIEW_TOP'))
             throw new ForbiddenException();
-
-        $this->loadModel('Vote.Vote');
         $this->layout = 'admin';
-
-        $this->set('thism', date('m'));
-        $month = array("janvier","fevrier","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","decembre");
-        $this->set('months', $month);
-
-        $new_month = array("Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre");
-        $this->set('new_months', $new_month);
         $this->set('title_for_layout', $this->Lang->get('VOTE__ADMIN_VIEW_TOP'));
-        for($i = 0; $i < 13; $i++){
-            if($i < 10){
-                $month = "0{$i}"; 
-            } else { 
+        $this->loadModel('Vote.Vote');
+        $thism = date('m');
+        $months = array("janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre");
+        $new_months = array("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre");
+        $date = date('Y-m-d h:i:s');
+        for ($i = 0; $i < 13; $i++) {
+            if ($i < 10) {
+                $month = "0{$i}";
+            } else {
                 $month = $i;
             };
             $this_year[$i] = $this->Vote->find('all', [
                 'fields' => ['username', 'COUNT(id) AS count'],
                 'conditions' => ['created LIKE' => date('Y') . '-' . $month . '-%', 'Vote.deleted_at' => null],
                 'order' => 'count DESC',
-                'group' => 'username'
+                'group' => 'user_id'
             ]);
             $last_year[$i] = $this->Vote->find('all', [
                 'fields' => ['username', 'COUNT(id) AS count'],
                 'conditions' => ['created LIKE' => date('Y') - 1 . '-' . $month . '-%', 'Vote.deleted_at' => null],
                 'order' => 'count DESC',
-                'group' => 'username'
+                'group' => 'user_id'
             ]);
-        }
-        for($i = 0; $i < 13; $i++){
-            if($i < 10){
-                $month = "0{$i}"; 
-            } else { 
-                $month = $i;
-            };
+
             $vote_this_year[$i] = $this->Vote->find('all', [
                 'fields' => ['COUNT(id) AS count'],
                 'conditions' => ['created LIKE' => date('Y') . '-' . $month . '-%', 'Vote.deleted_at' => null],
@@ -53,50 +43,24 @@ class TopController extends VoteAppController
             ]);
         }
 
-        $this->set(compact("this_year"));
-        $this->set(compact("last_year"));
-        $this->set(compact("vote_this_year"));
-        $this->set(compact("vote_last_year"));
         // GRAPH
-        for($i = 0; $i < 12; $i++)
-        {
-            $nbr_this_year[$i] = 0;
-            $nbr_last_year[$i] = 0;
-            $iv = 0;
-            foreach($this_year[$i] as $count){
-                $nbr_this_year[$i-1]++;
-            }
-            foreach($last_year[$i] as $count){
-                $nbr_last_year[$i-1]++;
-            }
-        }
-        $this->set("nbr_this_year", $nbr_this_year);
-        $this->set("nbr_last_year", $nbr_last_year);
-        
-        for($i = 0; $i < 12; $i++){
-            $nbr_vote_this_year[$i] = $vote_this_year[$i+1][0][0]['count'];
-            $nbr_vote_last_year[$i] = $vote_last_year[$i+1][0][0]['count'];
-        }
+        for ($i = 0; $i < 12; $i++) {
+            $nbr_this_year[$i] = count($this_year[$i + 1]);
+            $nbr_last_year[$i] = count($last_year[$i + 1]);
 
-        $this->set("nbr_vote_this_year", $nbr_vote_this_year);
-        $this->set("nbr_vote_last_year", $nbr_vote_last_year);
+            $nbr_vote_this_year[$i] = $vote_this_year[$i + 1][0][0]['count'];
+            $nbr_vote_last_year[$i] = $vote_last_year[$i + 1][0][0]['count'];
+        }
 
         $maxvoteur_this_year = array_sum($nbr_this_year);
         $maxvoteur_last_year = array_sum($nbr_last_year);
-        for($i = 0; $i < 12; $i++)
-        {
-			if($maxvoteur_last_year != 0){		
-				$percent_this_year[$i] = $nbr_this_year[$i]/$maxvoteur_this_year * 100;
-				$percent_last_year[$i] = $nbr_last_year[$i]/$maxvoteur_last_year * 100;
-			}
-			else {
-				$percent_this_year[$i] = $nbr_this_year[$i]/1 * 100;
-				$percent_last_year[$i] = $nbr_last_year[$i]/1 * 100;
-			}
+        for ($i = 0; $i < 12; $i++) {
+            $percent_this_year[$i] = $nbr_this_year[$i] / (($maxvoteur_last_year != 0) ? $maxvoteur_this_year : 1) * 100;
+            $percent_last_year[$i] = $nbr_last_year[$i] / (($maxvoteur_last_year != 0) ? $maxvoteur_last_year : 1) * 100;
+
         }
-        
-        $this->set("percent_this_year", $percent_this_year);
-        $this->set("percent_last_year", $percent_last_year);
+
+        $this->set(compact("thism", "months", "new_months", "this_year", "last_year", "vote_this_year", "vote_last_year", "nbr_this_year", "nbr_last_year", "nbr_vote_this_year", "nbr_vote_last_year", "percent_this_year", "percent_last_year"));
     }
     
     public function admin_del_vote()
