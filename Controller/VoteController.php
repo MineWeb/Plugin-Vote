f96298c513cfd2e85768a7e6985e66bc469a249a
0ddlyoko <0ddlyokoOfficial@gmail.com>
2020-02-29 23:02:01 +0100

    Wops

--- Controller/VoteController.php
+++ Controller/VoteController.php
@@ -276,7 +276,7 @@ class VoteController extends VoteAppController {
         // Give it
         $collectedVotesByServer = [];
         $collectedError = [];
-		$oldmoney = $money = floatval($this->User->getKey('money'));
+	$oldmoney = $money = floatval($this->User->getKey('money'));
         foreach ($votesList as $vote) {
             $reward = $vote['Reward'];
             if (!$this->Reward->collect($reward, $vote['Website']['server_id'], $this->User->getKey('pseudo'), $this->Server)) {
@@ -288,13 +288,13 @@ class VoteController extends VoteAppController {
                 $collectedVotesByServer[$server_id] = [];
             $collectedVotesByServer[$server_id][] = $vote;
             // Add money
-			// It seams to bugs when a player try to retrieve his reward and there is at least two money reward so we update money later
+            // It seams to bugs when a player try to retrieve his reward and there is at least two money reward so we update money later
             if ($reward['amount'] > 0)
-				$money += floatval($reward['amount']);
+                $money += floatval($reward['amount']);
         }
-		// Update money here (Also prevent a lot of database edit)
-		if (floatval($oldmoney) != floatval($money))
-			$this->User->setKey('money', ($money + floatval($reward['amount'])));
+        // Update money here (Also prevent a lot of database edit)
+        if (floatval($oldmoney) != floatval($money))
+            $this->User->setKey('money', $money);
 
         foreach ($collectedVotesByServer as $server_id => $votes) {
             $command = str_replace('{REWARD_NUMBER}', count($votes), $this->__getConfig()->global_command_plural);
