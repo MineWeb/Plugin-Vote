5935d3f2e8e43cc83e68c8b94d410aae29c09383
ThomasFar06 <32244230+ThomasFar06@users.noreply.github.com>
2020-04-09 19:14:52 +0200

    Add minecraft-top.com (#60)
    * Add files via upload
    
    * Add files via upload

--- Model/Website.php
+++ Model/Website.php
@@ -11,6 +11,12 @@ class Website extends VoteAppModel
             case 'RPG-PARADIZE':
                 // TODO: Check OUT
                 break;
+			case 'MINECRAFT-TOP-ORG':
+                // Check with API
+                $result = @file_get_contents("https://api.minecraft-top.com/v1/vote/$ip/{$website['data']['server_id']}");
+                if($result['vote'])
+                    return true;
+                break;
             case 'SRV-MC-ORG':
                 // Check with API
                 $result = @file_get_contents("http://www.serveurs-minecraft.org/api/is_valid_vote.php?id={$website['data']['server_id']}&ip=$ip&duration=3");
