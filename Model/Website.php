9aaead08c49433d75b0863ed00016408370ea44f
William AVENTIN <foy33@hotmail.fr>
2020-12-05 14:57:09 +0100

    Ajout serveurs-mc.net

--- Model/Website.php
+++ Model/Website.php
@@ -11,6 +11,11 @@ class Website extends VoteAppModel
             case 'RPG-PARADIZE':
                 // TODO: Check OUT
                 break;
+            case 'SERVEURS-MC':
+                // Check with API
+                $result = @file_get_contents("https://serveurs-mc.net/api/hasVote/{$website['data']['server_id']}/$ip/10");
+                $obj = json_decode($result);
+                return $obj->hasVote = "true";
 			case 'MINECRAFT-TOP-ORG':
                 // Check with API
                 $result = @file_get_contents("https://api.minecraft-top.com/v1/vote/$ip/{$website['data']['server_id']}");
