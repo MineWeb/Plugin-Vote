732709542616cbc6a48243b0cc49337abb573a6d
WIBOR <74981276+WIBORR@users.noreply.github.com>
2022-06-25 14:55:28 +0200

    Passage vérification yserveur.fr en json

--- Model/Website.php
+++ Model/Website.php
@@ -14,7 +14,8 @@ class Website extends VoteAppModel
             case 'YSERVEUR':
                 // Check with API
 		$result = @file_get_contents("https://yserveur.fr/api/vote/{$website['data']['server_id']}/$ip");
-                return $result == "true";
+                $obj = json_decode($result);
+                return $obj->vote == true;
             case 'SERVEURS-MC':
                 // Check with API
                 $result = @file_get_contents("https://serveurs-mc.net/api/hasVote/{$website['data']['server_id']}/$ip/10");
